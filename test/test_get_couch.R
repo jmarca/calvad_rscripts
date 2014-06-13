51c3f0f636c0a9bb9a9bb8cfc117b977e6af5991
James E. Marca <jmarca@translab.its.uci.edu>
2014-06-12 18:25:41 -0700

    added test for getting attachment stuff
    modified:   test/test_get_couch.R

--- test/test_get_couch.R
+++ test/test_get_couch.R
@@ -18,3 +18,34 @@ test_that("get.wim.vds.pairs() will get all pairs for a given year", {
     expect_that(dim(w.v.p),equals(c(78,5)))
 
 })
+
+test_that("get.Rdata.view works okay",{
+
+    vdsid <- 1114696
+    year <- 2010
+    paired.attachment <- get.RData.view(vdsid,year)
+    expect_that(paired.attachment,equals("wim.37.S.vdsid.1114696.2010.paired.RData"))
+
+    vdsid <- 1114694
+    year <- 2010
+    paired.attachment <- get.RData.view(vdsid,year)
+    expect_that(paired.attachment,is_equivalent_to(list()))
+
+})
+
+## this is really another bit of library, but so what
+## high level function is used here
+test_that("can get attached RData file okay",{
+
+    vdsid <- 1114696
+    year <- 2010
+    paired.attachment <- get.RData.view(vdsid,year)
+    result <- couch.get.attachment('vdsdata%2ftracking'
+                                   ,vdsid
+                                   ,paired.attachment
+                                   ,local=FALSE)
+    expect_that(is.null(df.merged),is_false())
+    expect_that(is.data.frame(df.merged),is_true())
+    expect_that(dim(df.merged),equals(c(8755,37)))
+
+})
