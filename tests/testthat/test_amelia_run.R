38a432df534f9aef18270ce650de71183e915f6d
James Marca <jmarca@translab.its.uci.edu>
2015-04-21 16:28:12 -0700

    switch to a different detector, round 2 amelia tweaks

--- tests/testthat/test_amelia_run.R
+++ tests/testthat/test_amelia_run.R
@@ -1,8 +1,8 @@
 test_that("plotting imputed data code works okay",{
 
-    file  <- './files/718204_ML_2012.df.2012.RData'
-    fname <- '718204_ML_2012'
-    vds.id <- 718204
+    file  <- './files/763346_ML_2012.df.2012.RData'
+    fname <- '763346_ML_2012'
+    vds.id <- 763346
     year <- 2012
     seconds <- 120
     path <- '.'
@@ -12,21 +12,10 @@ test_that("plotting imputed data code works okay",{
     df$ts <- NULL
     seconds=120
     df.agg <- vds.aggregate(df,ts,seconds=seconds)
+    files.to.couch <- plot_vds.data(df.agg,vds.id,2012,'raw','\npre imputation',force.plot=TRUE)
 
-    expect_that(dim(df.agg),equals(c(263520,12)))
-    expect_that(length(df.agg$nl1[is.na(df.agg$nl1)]),equals(57728))
-
-    expect_that(names(df.agg),
-                equals(c("ts", "nl1","nr3","nr2","nr1",
-                         "ol1", "or3","or2","or1",
-                         "obs_count","tod","day")))
 
     ## use sprintf("%0.10f",mean(df.agg$nl1,na.rm=TRUE)) to get long
-    ## decimal places
-    expect_that(min(df.agg$nl1,na.rm=TRUE),equals(0.0))
-    expect_that(median(df.agg$nl1,na.rm=TRUE),equals(43.0))
-    expect_that(mean(df.agg$nl1,na.rm=TRUE),equals(40.7003479241))
-    expect_that(max(df.agg$nl1,na.rm=TRUE),equals(101))
 
     lanes <- longway.guess.lanes(df)
     n.idx <- vds.lane.numbers(lanes,c("n"))
@@ -35,6 +24,7 @@ test_that("plotting imputed data code works okay",{
     o.bds.len <- length(o.cols)
     o.bds <- matrix(c(o.cols,sort( rep(c(0, 1),o.bds.len))), nrow = o.bds.len, byrow=FALSE)
 
+    
 
     df.vds.agg.imputed <- NULL
     maxiter <-  20
@@ -60,38 +50,10 @@ test_that("plotting imputed data code works okay",{
     ## expect_that(res,equals('df.vds.agg.imputed'))
 
     df.merged <- condense.amelia.output(df.vds.agg.imputed)
-    expect_that(dim(df.merged),equals(c(8784,12)))
-
-    varnames <- names(df.merged)
-    expect_that(varnames,equals(c("ts", "nl1","nr3","nr2","nr1",
-                                  "ol1", "or3","or2","or1",
-                                  "obs_count","tod","day")))
-
-    ## expect_that(min(df.merged$nl1),equals(0.0))
-    ## expect_that(median(df.merged$nl1),equals(1331.2886693577))
-    ## expect_that(mean(df.merged$nl1),less_than(1300))
-    ## expect_that(max(df.merged$nl1),less_that(2500))
-
-### that should be good enough to verify that condense hasn't changed
-### its spots
-
     twerked.df <- recode.df.vds(df.merged)
-
-    expect_that(dim(twerked.df),equals(c(35136,7)))
-    expect_that(names(twerked.df),equals(c("ts","tod","day",
-                                          "obs_count","lane","volume",
-                                          "occupancy")))
-
-    expect_that(levels(twerked.df$lane),equals(c( "left",   "lane 2", "lane 3", "right" )))
-
-    files.to.couch <- plot.vds.data(df.merged,718204,2012,'imputed','\npost imputation',force.plot=TRUE)
+    files.to.couch <- plot.vds.data(df.merged,vds.id,2012,'imputed','\npost imputation',force.plot=TRUE)
 
     expect_that(files.to.couch,equals(
-        c("images/718204/718204_2012_imputed_001.png",
-          "images/718204/718204_2012_imputed_002.png",
-          "images/718204/718204_2012_imputed_003.png",
-          "images/718204/718204_2012_imputed_004.png"
-          )))
-
-    ## should also md5 check the dumped images?
+        paste("images/",vds.id,"/",vds.id,'_',year,'imputed_00',c(1,2,3,4),'.png',sep='')
+        ))
 })
