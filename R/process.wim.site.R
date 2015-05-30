78231f07302db8452e08cc327cc134031cd2d6d7
James E. Marca <james@activimetrics.com>
2015-05-30 09:22:07 -0700

    trying to fix the lane numbering for the WIM sites
    plotter lane translation was a hack and broked.

--- R/process.wim.site.R
+++ R/process.wim.site.R
@@ -576,38 +576,34 @@ recode.df.wim <- function( df ){
                          labels=c('Su','Mo','Tu','We','Th','Fr','Sa'))
 
     ## lanes?
+    lanes <- extract_unique_lanes(df)
 
-    lanes <- levels(as.factor(melded$lane))
+    ## the lanes for a WIM site might not include all lanes.  For
+    ## example, site 52 has 4 lanes, but data only for 2,3,4 (aka
+    ## r3,r2,r1, in order), not data for the left lane (lane 1, aka
+    ## l1)
 
-    ## this is a hack, is stupid, and is wrong
-    ## Often with WIM sites, there are in fact multiple right lanes and no data in the left lane
-    ## lane.names <- c("left","right")
-    ## if(length(lanes)==1){
-    ##     lane.names <- c("right")
-    ## }else{
-    ##     numbering <- length(lanes)
-    ##     if(length(lanes)>2){
-    ##         for(i in 3:numbering){
-    ##             lane.names[i]=paste("lane",(numbering-i+2))
-    ##         }
-    ##         ## a little switcheroo here
-    ##         lanes <- c(lanes[1],rev(lanes[-1]))
-    ##         lane.names <- c(lane.names[1],rev(lane.names[-1]))
-    ##     }
-    ## }
-
-    ## instead, iterate over each "level" of lanes.  if it has an R, then make it a right lane.  If it has an l1, then make it lane 1.
-    ##
-    ## I guess two passes will work.  One to suss out left right lanes, max right lane number, second to assign.
-    maxright <- 0
-    haveleft <- FALSE
-    for (l in lanes){
-
+    rightlanes <- NULL
+    rightlabels <- NULL
+    leftlane <- c('l1')
+    leftlabel <- c('left')
+    for(lane in  rev(sort(lanes))){
+        if(lane == 'l1'){
+            ## last time through no matter what
+            next
+        }
+        ## but there might not be a left lane...
+        rightlanes <- c(rightlanes,lane)
+        if(lane == 'r1'){
+            rightlabels <- c(rightlabels,'right')
+        }else{
+            rightlabels <- c(rightlabels,paste('lane',length(rightlabels)+2))
+        }
     }
 
     melded$lane <- factor(melded$lane,
-                          levels=lanes,
-                          labels=lane.names)
+                          levels=c('l1',rightlanes),
+                          labels=c('left',rightlabels))
 
     melded
 
