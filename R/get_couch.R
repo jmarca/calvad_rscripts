2ca76b8cc30b629e63a59289bec838c4431b80c6
James E. Marca <james@activimetrics.com>
2015-07-09 23:33:42 -0700

    additions and fixes to get_couch.R
    fixed the way I process query options
    
    added get.vds.paired.to.wim (so if you know WIM you can get VDS with the
    paired doc attachment, if any)
    
    fixed the tests

--- R/get_couch.R
+++ R/get_couch.R
@@ -20,14 +20,16 @@
 ##' @return a data frame with year, vds_id, wim_id, doc
 ##' @author James E. Marca
 get.vds.wim.pairs <- function(year,trackingdb='vdsdata%2ftracking'){
+    view_query <- list(
+        'startkey'=list(paste(year))
+       ,'endkey' =list(paste(year+1))
+       ,'reduce'=FALSE)
+
     docs <- rcouchutils::couch.allDocs(db=trackingdb
-                                      ,query=list(
-                            'startkey'=paste('%5b%22',year,'%22','%5d',sep='')
-                            ,'endkey' =paste('%5b%22',year+1,'%22','%5d',sep='')
-                            ,'reduce'='false')
+                                      ,query=view_query
                                       ,view='_design/vds/_view/pairRData'
                                       ,include.docs = FALSE)
-  rows <- docs$rows
+    rows <- docs$rows
   records <- sapply(rows,function(r){
       ## parse out wim info
       x = r$key[[3]]
@@ -53,6 +55,66 @@ get.vds.wim.pairs <- function(year,trackingdb='vdsdata%2ftracking'){
   df.pairs
 }
 
+##' get vds wim pair for a WIM site from couchdb
+##'
+##' get the design doc wim/pairRData for the passed in year
+##'
+##' The result will be the paired vds_id and wim_id, or nothing
+##'
+##' @title get.vds.paired.to.wim
+##' @param year the year
+##' @param site_no the wim site number
+##' @param direction the direction
+##' @param trackingdb the db to check, the usual "vdsdata\%2ftracking"
+##' is default goto db here
+##' @return a data frame with year, vds_id, wim_id, doc
+##' @author James E. Marca
+##' @export
+##'
+get.vds.paired.to.wim <- function(year,
+                                  site_no,
+                                  direction,
+                                  trackingdb='vdsdata%2ftracking'){
+    cdb.wimid <- paste('wim',site_no,direction,sep='.')
+
+    view_query <- list(
+        'startkey'=list(year,cdb.wimid),
+        'endkey' =list(year,paste(cdb.wimid,'\ufff0',sep='')),
+        'reduce'=FALSE)
+
+    docs <- rcouchutils::couch.allDocs(db=trackingdb
+                                      ,query=view_query
+                                      ,view='_design/wim/_view/pairRData'
+                                      ,include.docs = FALSE)
+
+    rows <- docs$rows
+    records <- sapply(rows,function(r){
+        ## fix things up a bit
+        x = r$key[[3]]
+        m <- regexec("^wim\\.([0-9]+)\\.([NSEW])\\.vdsid\\.(\\d+)\\.",x)
+        wim.info <- regmatches(x,m)[[1]]
+        return (list('year'=as.numeric(r$key[[1]]),
+                     'vds_id'=as.numeric(wim.info[4]),
+                     'attachment'=r$key[[3]],
+                     'cdb.wimid'=paste('wim',wim.info[2],wim.info[3],sep='.'),
+                     'site_no'=as.integer(wim.info[2]),
+                     'direction'=wim.info[3]
+                     ))
+    })
+  if(length(records)==0){
+      return(data.frame())
+  }
+  ## convert to a dataframe
+  df.pairs <- data.frame(year=unlist(records[1,])
+                        ,vds_id=unlist(records[2,])
+                        ,cdb.wimid=unlist(records[4,])
+                        ,site_no=unlist(records[5,])
+                        ,direction=unlist(records[6,])
+                        ,doc=unlist(records[3,])
+                        ,stringsAsFactors=FALSE)
+  df.pairs
+}
+
 ##' get RData view
 ##'
 ##' I sometimes store RData as attachments to docs in couchdb
@@ -69,16 +131,16 @@ get.RData.view <- function(vdsid,year,trackingdb='vdsdata%2ftracking'){
     docs <- rcouchutils::couch.allDocs(
         trackingdb
        ,query=list(
-            'startkey'=paste('%5b%22',year,'%22,%22',vdsid,'%22%5d',sep='')
-           ,'endkey' =paste('%5b%22',year,'%22,%22',vdsid,'%22,%5b%5d%5d',sep='')
-           ,'reduce'='false')
+            'startkey'=list(paste(year),paste(vdsid))
+           ,'endkey' =list(paste(year),paste(vdsid,'\ufff0%5d',sep=''))
+           ,'reduce'=FALSE)
       , view='_design/vds/_view/pairRData'
       , include.docs = FALSE)
-  rows <- docs$rows
-  files <- sapply(rows,function(r){
-    return (r$key[[3]])
-  })
-  files
+    rows <- docs$rows
+    files <- sapply(rows,function(r){
+        return (r$key[[3]])
+    })
+    files
 }
 
 ##' Record unmet imputation conditions in CouchDB
--- tests/testthat/test_get_couch.R
+++ tests/testthat/test_get_couch.R
@@ -18,6 +18,17 @@ test_that("get.wim.vds.pairs() will get all pairs for a given year", {
 
 })
 
+test_that("get.vds.paired.to.wim() works okay", {
+
+    year <- 2012
+    w.v.p <- get.vds.paired.to.wim(year,site_no=66,direction='S')
+    expect_that(is.data.frame(w.v.p), is_true())
+    expect_that(w.v.p$year,equals(2012))
+    expect_that(w.v.p$cdb.wimid,equals('wim.66.S'))
+    expect_that(w.v.p$doc,equals('wim.66.S.vdsid.821071.2012.paired.RData'))
+
+})
+
 test_that("get.Rdata.view works okay",{
 
     vdsid <- 1114696
@@ -40,91 +51,18 @@ test_that("can get attached RData file okay",{
     vdsid <- 1114696
     year <- 2010
     paired.attachment <- get.RData.view(vdsid,year)
-    result <- couch.get.attachment('vdsdata%2ftracking'
-                                   ,vdsid
-                                   ,paired.attachment
-                                   ,local=FALSE)
-    expect_that(result,equals("df.merged"))
-    expect_that(is.null(df.merged),is_false())
-    expect_that(is.data.frame(df.merged),is_true())
-    expect_that(dim(df.merged),equals(c(8755,37)))
+    result <- rcouchutils::couch.get.attachment('vdsdata%2ftracking'
+                                               ,vdsid
+                                               ,paired.attachment
+                                                )
+    expect_that(result,is_a('list'))
 
-})
+    varnames <- names(result)[1]
+    expect_that(varnames,equals('df.merged'))
 
-## this test checks being able to filter out lanes
-##
-## same exact parameters in the setup stageas the above test, because
-## it that works, then this will work
-test_that("can filter out lanes from merged dataframe",{
-    vdsid <- 1114696
-    year <- 2010
-    paired.attachment <- get.RData.view(vdsid,year)
-    result <- couch.get.attachment('vdsdata%2ftracking'
-                                   ,vdsid
-                                   ,paired.attachment
-                                   ,local=FALSE)
+    barfl <- result[[1]][[varnames]]
 
-    paired.names <- sort(names(df.merged))
-    ## this set originally has three lanes in vds data, two lanes in
-    ## WIM truck data, plus a third lane in report data for speeds
-
-    ## in this case, I expect the exact same column names in both sets
-    df.trimmed <- evaluate.paired.data(df.merged,3,4)
-    expect_that(is.data.frame(df.merged),is_true())
-    expect_that(dim(df.trimmed),equals(c(8755,37)))
-    expect_that(sort(names(df.trimmed)),equals(paired.names))
-
-    ## now fewer lanes
-    df.trimmed <- evaluate.paired.data(df.merged,2,4)
-    ## in this case, I expect the exact same column names in both sets
-    expect_that(is.data.frame(df.merged),is_true())
-    expect_that(dim(df.trimmed),equals(c(8755,35)))
-    expected.names <- c(
-        "nl1"                      ,"nr3"
-        ,"nr2"                      ,"nr1"
-        ,"ol1"                      ,"or3"
-        ,"or2"                      ,"or1"
-        ,"not_heavyheavy_r1"        ,"heavyheavy_r1"
-        ,"hh_weight_r1"             ,"hh_axles_r1"
-        ,"hh_len_r1"                ,"hh_speed_r1"
-        ,"nh_weight_r1"             ,"nh_axles_r1"
-        ,"nh_len_r1"                ,"nh_speed_r1"
-        ,"not_heavyheavy_r2"        ,"heavyheavy_r2"
-        ,"hh_weight_r2"             ,"hh_axles_r2"
-        ,"hh_len_r2"                ,"hh_speed_r2"
-        ,"nh_weight_r2"             ,"nh_axles_r2"
-        ,"nh_len_r2"                ,"nh_speed_r2"
-        ,"wgt_spd_all_veh_speed_r1" ,"count_all_veh_speed_r1"
-        ,"wgt_spd_all_veh_speed_r2" ,"count_all_veh_speed_r2"
-        ,"ts"                       ,"tod"
-        ,"day")
-    expect_that(sort(names(df.trimmed)),equals(sort(expected.names)))
-
-    ## now just 2 lanes of general flow
-    df.trimmed <- evaluate.paired.data(df.merged,2,2)
-    ## in this case, I expect the exact same column names in both sets
-    expect_that(is.data.frame(df.merged),is_true())
-    expect_that(dim(df.trimmed),equals(c(8755,31)))
-    expected.names <- c(
-        "nl1"
-        ,"nr1"
-        ,"ol1"
-        ,"or1"
-        ,"not_heavyheavy_r1"        ,"heavyheavy_r1"
-        ,"hh_weight_r1"             ,"hh_axles_r1"
-        ,"hh_len_r1"                ,"hh_speed_r1"
-        ,"nh_weight_r1"             ,"nh_axles_r1"
-        ,"nh_len_r1"                ,"nh_speed_r1"
-        ,"not_heavyheavy_r2"        ,"heavyheavy_r2"
-        ,"hh_weight_r2"             ,"hh_axles_r2"
-        ,"hh_len_r2"                ,"hh_speed_r2"
-        ,"nh_weight_r2"             ,"nh_axles_r2"
-        ,"nh_len_r2"                ,"nh_speed_r2"
-        ,"wgt_spd_all_veh_speed_r1" ,"count_all_veh_speed_r1"
-        ,"wgt_spd_all_veh_speed_r2" ,"count_all_veh_speed_r2"
-        ,"ts"                       ,"tod"
-        ,"day")
-    expect_that(sort(names(df.trimmed)),equals(sort(expected.names)))
-
+    expect_that(is.data.frame(barfl),is_true())
+    expect_that(dim(barfl),equals(c(8755,37)))
 
 })
