87ad746dbb01cc56d092ccf3ab1dc9a1e20803c2
James E. Marca <james@activimetrics.com>
2017-07-17 15:57:33 -0700

    commit at a midpoint
    tams processing works up through plotting raw data

--- R/load.tams.from.csv.R
+++ R/load.tams.from.csv.R
@@ -104,6 +104,144 @@ tams.lane.and.time.aggregation <- function(lane.dir.data){
     ## to be refactored
 
 }
+
+timestamp_insanity_fix <- function(df){
+
+    ## make sure df is sorted by id, not time
+    id_order <- order(df$sig_id)
+    df <- df[id_order,]
+    ## expect hrly exists and is what I want
+    hrly <- df$hrly
+
+    ## expect time to monotonically increase.
+    ## that means any repeated hours is a bad thing (TM)
+
+    ## step through the times.  If hour goes backwards, flag it as bad
+    len <- length(hrly)
+    time_shifts <- hrly[-1] - hrly[-len]
+    hr_shifts <- c(hrly[1],hrly[-1][time_shifts != 0])
+    temp_df <- tibble::tibble(hr=hr_shifts,n=1)
+
+    sqlstatement <- paste("select min(hr) as hr",
+                          ', total(1) as cnt',
+                          'from temp_df group by hr order by hr',
+                          sep=' ',collapse=' '
+                          )
+    result <- sqldf::sqldf(sqlstatement,drv="SQLite")
+    dupes <- result$cnt>1
+
+    ## keepers <- ! is.element(hrly,result$hr[dupes])
+    ## keepers
+    result$hr[dupes]
+}
+
+##' Do the work of calculating the timestamps affected by the day fail
+##' to roll over bug
+##'
+##' Do the work of calculating the timestamps affected by the day fail
+##' to roll over bug
+##' @title off_timestamp
+##' @param times the vector of times to inspect
+##' @param offset positive looks for too soon a day jump, negative
+##'     looks for too late a day jump
+##' @return an index of the ones that are a day off.  If all false,
+##'     then you are done
+##' @author James E. Marca
+off_timestamp <- function(times,offset=-23){
+    last.idx <- length(times)
+    timestamp_diff <- as.numeric(times[-1] - times[-last.idx],units="hours")
+    bad_time <- NULL
+    round_midnight <- NULL
+    ts.lt <- as.POSIXlt(times)
+
+    if(offset < 0){
+        round_midnight <- (ts.lt$hour[-1] == 0  & ts.lt$min[-1] == 0)
+        bad_time <- timestamp_diff < -23 & timestamp_diff > -25
+    }else{
+        round_midnight <- (ts.lt$hour[-1] == 23 & ts.lt$min[-1] == 59)
+        bad_time <- timestamp_diff > 23 & timestamp_diff < 25
+    }
+
+    ten.or.less <- min(10,length(bad_time[bad_time & round_midnight]))
+    print(paste('got',length(bad_time[bad_time & round_midnight]),'problem entries'))
+    if(length(bad_time[bad_time & round_midnight])>0){
+        print('first few erroneous entries')
+        print(times[-1][bad_time & round_midnight][1:ten.or.less])
+        print('entries immediately preceding bad entries')
+        print(times[bad_time & round_midnight][1:ten.or.less])
+    }
+    bad_time & round_midnight
+}
+
+##' Do the work of calculating the timestamps affected by the day fail
+##' to roll over bug
+##'
+##' Do the work of calculating the timestamps affected by the day fail
+##' to roll over bug
+##' @title day_back_timestamp
+##' @param times the vector of times to inspect
+##' @return an index of the ones that are a day off.  If all false,
+##'     then you are done
+##' @author James E. Marca
+day_back_timestamp <- function(times){
+    off_timestamp(times,-23)
+}
+
+
+##' Do the work of calculating the timestamps affected by the day fail
+##' to roll over bug
+##'
+##' Do the work of calculating the timestamps affected by the day fail
+##' to roll over bug
+##' @title day_forward_timestamp
+##' @param times the vector of times to inspect
+##' @return an index of the ones that are a day off.  If all false,
+##'     then you are done
+day_forward_timestamp <- function(times){
+    off_timestamp(times,23)
+}
+
+
+##' date_rollover_bug
+##' A function to fix the cases where the date doesn't increment
+##' properly but the hour:minute do switch over to 00:00
+##'
+##' This function should be passed the almost pure CSV data.  The only
+##' modification should be that the directions are uniform (I'm going
+##' to expect all lane_dir is the same).  The records will be checked
+##' to make sure that the date increments properly when it should.
+##' @title date_rollover_bug
+##' @param tams the tams csv data, but only for one direction, not both
+##' @return tams a replacement df, with the dates corrected
+##' @author James E. Marca
+date_rollover_bug <- function(times){
+
+    ## time by lane and direction should be uniformly increasing
+    ## except it doesn't always.  The only real bug is when it rolls
+    ## over the end of the day, resets the hour to 00, but doesn't
+    ## properly increment the date.  Sadly there might be a few of
+    ## these.
+
+    loopcount <- 0
+    positive_time <- day_forward_timestamp(times)
+    while(length(positive_time[positive_time]) > 0 && loopcount < 10){
+        print(loopcount <- loopcount + 1)
+        times[-1][positive_time] <- times[-1][positive_time] - 24 * 60 * 60
+        negative_time <- day_back_timestamp(times)
+        if(length(negative_time)>0){
+            times[-1][negative_time] <- times[-1][negative_time] + 24 * 60 * 60
+        }
+        positive_time <- day_forward_timestamp(times)
+    }
+    negative_time <- day_back_timestamp(times)
+    while(length(negative_time[negative_time])>0 && loopcount < 10){
+        print(loopcount <- loopcount + 1)
+        times[-1][negative_time] <- times[-1][negative_time] + 24 * 60 * 60
+        negative_time <- day_back_timestamp(times)
+    }
+    times
+}
+
 ##' reshape.tams.from.csv
 ##' Take tams raw CSV, and turn it into one tibble per direction
 ##'
@@ -116,17 +254,37 @@ tams.lane.and.time.aggregation <- function(lane.dir.data){
 ##' @author James E. Marca
 ##' @export
 ##'
-reshape.tams.from.csv <- function(tams.csv,tams.path,year=0,trim.to.year=TRUE){
+reshape.tams.from.csv <- function(tams.csv,tams.path,year,trim.to.year=TRUE){
     ## prep for sqldf if you need postgresql
     # config <- rcouchutils::get.config()
     # sqldf_postgresql(config)
 
+    ## make sure order is correct.  No guarantee that files are read
+    ## in in sig_id ordering
+    ## o <- order(tams.csv$sig_id)
+    ## tams.csv <- tams.csv[o,]
+
+    ## extract some info
     tams.site <- tams.csv$detstaid[1]
-    site.lanes <- max(tams.data.csv$lane)
+    number.lanes <- max(tams.data.csv$lane)
+
     hour <-  3600 ## seconds per hour
-    tams.csv$hourly <- as.numeric(tams.csv$timestamp_full) - as.numeric(tams.csv$timestamp_full) %% hour
 
     ts.ct   <- as.POSIXct(tams.csv$timestamp_full,tz='UTC')
+
+    if(trim.to.year){
+        ## trim any records not in the requested year
+        print(paste('trimming to year',year))
+        keep.records <- (1900 + as.POSIXlt.date(ts.ct)$year) ==  year
+        ## print(summary(keep.records))
+        ## print('length original')
+        ## print(dim(df.return))
+        ## print('length keep')
+        ## print(dim(df.return[keep.records,]))
+        tams.csv <- tams.csv[keep.records,]
+        ts.ct <- ts.ct[keep.records]
+    }
+
     tams.csv$hrly <- as.numeric(trunc(ts.ct,"hours"))
     tams.csv <- tams.recode.lanes(tams.recode.lane_dir(tams.csv))
     tams.csv$any_vehicle <- 1
@@ -135,28 +293,44 @@ reshape.tams.from.csv <- function(tams.csv,tams.path,year=0,trim.to.year=TRUE){
     tams.csv$heavyheavy[tams.csv$calvad_class == 'HHDT'] <- 1
     tams.csv$not_heavyheavy[tams.csv$calvad_class == 'NHHDT'] <- 1
     ##if(length(levels(tams.csv$lane_dir)) > 1)
-    tams.by.dir <- split(tams.csv,tams.csv$lane_dir)
 
-
-    directions <- levels(tams.csv$lane_dir)
     mean.var.names <- c('not_heavyheavy','heavyheavy')
+
+    tams.by.dir <- split(tams.csv,tams.csv$lane_dir)
+    directions <- names(tams.by.dir)
+    bad_hrs_set <- c()
+
     for(direction in directions){
         print(paste('processing',tams.site,direction))
         cdb.tamsid <- paste('tams',tams.site,direction,sep='.')
         tams <- tams.by.dir[[direction]]
-
         if(length(dim(tams)) !=  2){
             ## probably have an issue here
             print(paste("no data loaded for",tams.site,direction," ---  skipping"))
             next
         }
-        ## eventuall stick preplot stuff here
 
+        ## preplot prior to cleaning??
+        tams.data.hr.lane <- split(tams,tams$lane)
+        lanes <- names(tams.data.hr.lane)
 
-        tams.data.agg <- split(tams,tams$lane)
-        for (l in names(tams.data.agg)){
-            temp_df <- tams.data.agg[[l]]
-            sqlstatement2 <- paste("select min(timestamp_full) as ts,",
+        for (l in lanes){
+            temp_df <- tams.data.hr.lane[[l]]
+
+            ## fix the date rollover bug
+            fixed_times <- date_rollover_bug(temp_df$timestamp_full)
+            temp_df$timestamp_full <- fixed_times
+            ts.ct   <- as.POSIXct(fixed_times,tz='UTC')
+            temp_df$hrly <- as.numeric(trunc(ts.ct,"hours"))
+            bad_hrs <- timestamp_insanity_fix(temp_df)
+            bad_hrs_set <- union(bad_hrs_set,bad_hrs)
+            keepers <- ! is.element(temp_df$hrly,bad_hrs)
+            drops_num <- length(keepers[!keepers])
+            prior_num <- length(keepers)
+            print(paste('dropping',drops_num,'out of',prior_num,'total records (or',round(100*(drops_num/prior_num),2),'percent) due to duplicated hours'))
+            temp_df <- temp_df[keepers,]
+
+            sqlstatement2 <- paste("select min(hrly) as ts,",
                                    paste('total(',
                                          c(mean.var.names),
                                          ') as ',
@@ -167,24 +341,20 @@ reshape.tams.from.csv <- function(tams.csv,tams.path,year=0,trim.to.year=TRUE){
                                          collapse=', '),
                                    ', total(any_vehicle) as ',
                                    paste('n',l,sep='_'),
-                                   ', max(vehicle_count) as ',
-                                   paste('maxn',l,sep='_'),
-                                   ', min(vehicle_count) as ',
-                                   paste('minn',l,sep='_'),
                                    'from temp_df group by hrly',
                                    sep=' ',collapse=' '
                                    )
             df_hourly <- sqldf::sqldf(sqlstatement2,drv="SQLite")
             df_hourly$ts <- as.POSIXct(df_hourly$ts,origin = "1970-01-01", tz = "GMT")
             df_hourly$ts <- trunc(df_hourly$ts,units='hours')
-            tams.data.agg[[l]] <- df_hourly
+            tams.data.hr.lane[[l]] <- df_hourly
         }
         print('combine lane by lane aggregates')
         ## combine lane by lane aggregates by same hour
-        df.return <- tams.data.agg[[1]]
-        if(length(tams.data.agg)>1){
-            for(l in 2:length(tams.data.agg)){
-                df.return <- merge(df.return,tams.data.agg[[l]],all=TRUE)
+        df.return <- tams.data.hr.lane[[1]]
+        if(length(tams.data.hr.lane)>1){
+            for(l in 2:length(tams.data.hr.lane)){
+                df.return <- merge(df.return,tams.data.hr.lane[[l]],all=TRUE)
             }
         }
         print(dim(df.return))
@@ -200,25 +370,25 @@ reshape.tams.from.csv <- function(tams.csv,tams.path,year=0,trim.to.year=TRUE){
         attr(ts.all.df$ts,'tzone') <- 'UTC'
         df.return <- merge(df.return,ts.all.df,all=TRUE)
 
-        if(year == 0){
-            about.the.middle <- floor(length(df.return$ts))
-            year <- 1900 + as.POSIXlt.date(df.return$ts[about.the.middle])$year
-        }
 
-        if(trim.to.year){
-            ## trim any records not in the requested year
-            print(paste('trimming to year',year))
-            keep.records <- (1900 + as.POSIXlt.date(df.return$ts)$year) ==  year
-            ## print(summary(keep.records))
-            ## print('length original')
-            ## print(dim(df.return))
-            ## print('length keep')
-            ## print(dim(df.return[keep.records,]))
-            df.return <- df.return[keep.records,]
-        }
 
         df.return <- add.time.of.day(df.return)
         tams.by.dir[[direction]] <- df.return
+
+
+    }
+
+    ## finally, do one more pass to drop any missed bad hours
+    for(direction in directions){
+        df.return <- tams.by.dir[[direction]]
+
+        hrly <- as.numeric(trunc(df.return$ts,"hours"))
+        keepers <- ! is.element(hrly,bad_hrs_set)
+        if(length(keepers[!keepers]) > 0){
+            print(paste('second pass, dropping',length(keepers[!keepers]),'records due to duplicated hours'))
+            df.return <- df.return[keepers,]
+            tams.by.dir[[direction]] <- df.return
+        }
 
         ## save tams data for next time
         filename <- paste(tams.site,direction,year,'tams.agg.RData',sep='.')
@@ -231,13 +401,11 @@ reshape.tams.from.csv <- function(tams.csv,tams.path,year=0,trim.to.year=TRUE){
         savepath <- paste(savepath,direction,sep='/')
         if(!file.exists(savepath)){dir.create(savepath)}
         filepath <- paste(savepath,filename,sep='/')
-
         save(df.return,file=filepath,compress='xz')
         print(paste('saved',tams.site,direction,'to',filepath))
-
     }
 
-    list(tams.by.dir,site.lanes)
+    list(tams.by.dir,number.lanes)
 }
 
 ##     df.tams <- load.tams.data.straight(tams.site=tams.site,year=year,con=con)
