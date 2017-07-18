7344c9384e2b7c63fbdc05bce790f2e7434c616d
James E. Marca <james@activimetrics.com>
2017-07-18 16:23:34 -0700

    deploy hacked fixes for installing r libs
    as with confir, deduce the right place by searching the current
    working directory for the first occurrence of node_modules.  Use that
    as top level directory for installing, so that all R libs go in the
    same place.  Hopefully will prevent multiple deploys of same libraries

--- Rinstall.R
+++ Rinstall.R
@@ -1,17 +1,25 @@
-dot_is <- paste(getwd(),'.',sep='/')
+envrr <- Sys.getenv()
+dot_is <- getwd()
 
+plaba <- regexpr(pattern='/node_modules',envrr['PWD'])
+if(plaba>0){
+    ## stoppoint <- plaba + 12 ## same as  attr(plaba,'match.length')
+    stoppoint <- plaba  ## actually, go one up
+    dot_is <- substr(envrr['PWD'],1,stoppoint)
+}
 node_paths <- dir(dot_is,pattern='\\.Rlibs',
                   full.names=TRUE,recursive=TRUE,
                   ignore.case=TRUE,include.dirs=TRUE,
                   all.files = TRUE)
 path <- normalizePath(paste(dot_is,'.Rlibs',sep='/')
                     , winslash = "/", mustWork = FALSE)
-if(!file.exists('path')){
+if(!file.exists(path)){
     dir.create(path)
 }
+
 lib_paths <- .libPaths()
 .libPaths(c(path,node_paths,lib_paths))
+
 ## ready to go
-devtools::install_deps()
 devtools::document()
 devtools::install()
